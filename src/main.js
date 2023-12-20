aadd48043c5e5c63b92f68e80dac797ca219ce56
hotwords123 <hotwords123@gmail.com>
2023-12-20 11:27:34 +0800

    feat: optimize logic of entering active lessons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import { GM_openInTab, unsafeWindow } from '$';
+import { GM_openInTab, GM_getTab, GM_saveTab, GM_getTabs, unsafeWindow } from '$';
 import './style.css';
 import App from './App.vue';
 import storage from './storage';
@@ -15,6 +15,12 @@ if (url.pathname.startsWith("/lesson/fullscreen/v3/")) {
 }
 
 function launchLessonHelper() {
+  GM_getTab((tab) => {
+    tab.type = "lesson";
+    tab.lessonId = url.pathname.split("/")[4];
+    GM_saveTab(tab);
+  });
+
   createApp(App).mount(
     (() => {
       const app = document.createElement('div');
@@ -27,26 +33,42 @@ function launchLessonHelper() {
 function pollActiveLessons() {
   const enteredLessonIds = new Set();
 
-  async function checkActiveLessons() {
-    try {
-      const resp = await API.getActiveLessons();
-      if (resp.code !== 0)
-        throw new Error(resp.msg);
-
-      for (const lesson of resp.data.onLessonClassrooms) {
-        const { classroomId, lessonId } = lesson;
-
-        if (!enteredLessonIds.has(lessonId)) {
-          GM_openInTab("https://pro.yuketang.cn/lesson/fullscreen/v3/" + lessonId);
-          enteredLessonIds.add(lessonId);
+  function updateLessonIds() {
+    return new Promise((resolve) => {
+      GM_getTabs((tabs) => {
+        for (const key in tabs) {
+          const tab = tabs[key];
+          if (tab.type === "lesson") {
+            enteredLessonIds.add(tab.lessonId);
+          }
         }
+        resolve();
+      });
+    });
+  }
+
+  async function checkActiveLessons() {
+    const resp = await API.getActiveLessons();
+    if (resp.code !== 0) {
+      throw new Error("Failed to get active lessons: " + resp.msg);
+    }
+
+    for (const lesson of resp.data.onLessonClassrooms) {
+      const { classroomId, lessonId } = lesson;
+
+      if (!enteredLessonIds.has(lessonId)) {
+        GM_openInTab("https://pro.yuketang.cn/lesson/fullscreen/v3/" + lessonId);
+        enteredLessonIds.add(lessonId);
       }
-    } catch (err) {
-      console.error(err);
     }
   }
 
-  setInterval(checkActiveLessons, 5000);
+  updateLessonIds();
+
+  setInterval(async () => {
+    await updateLessonIds();
+    await checkActiveLessons();
+  }, 5000);
 }
 
 unsafeWindow.WebSocket = MyWebSocket;
